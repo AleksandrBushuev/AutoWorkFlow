177d522b5985f4bd40ba9efcac5555805d71c839
Aleksandr Bushuev <aleksandr.bushuev@indusoft.ru>
2020-12-03 12:39:34 +0500

    Создал в консольном приложении метод, позволяющий выполнить проверку авторизации и получение информации по тикету

--- AutoWorkFlow/AutoWorkFlow/Program.cs
+++ AutoWorkFlow/AutoWorkFlow/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoWorkFlow.OTRS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,29 @@ namespace AutoWorkFlow
     {
         static void Main(string[] args)
         {
+            var baseAddress = "https://support.indusoft.ru/otrs/index.pl";
+
+
+            UserCredential credential = InputUserCredential();
+
+            IAutorizationService service = new AutorizationService(baseAddress, credential);
+            OtrsClient client = new OtrsClient(service);
+
+            OtrsTicketInfo info = client.GetTicketInfoAsync(baseAddress, "31627").Result;
+
+            Console.WriteLine(info.ToString());
+
+            Console.ReadKey();
+        }
+
+        static UserCredential InputUserCredential()
+        {
+            Console.WriteLine("Введите логин пользователя OTRS");
+            string login = Console.ReadLine();
+            Console.WriteLine("Введите логин пользователя OTRS");
+            string password = Console.ReadLine();
+
+            return new UserCredential(login, password);
         }
     }
 }
