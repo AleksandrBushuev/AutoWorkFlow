57b223295065891a4c621aa9d0ae723c0b692dae
Aleksandr Bushuev <aleksandr.bushuev@indusoft.ru>
2020-12-03 11:15:21 +0500

    Создал класс с необходимой информацией по тикету OTRS

---
+++ AutoWorkFlow/AutoWorkFlow.OTRS/OtrsTicketInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AutoWorkFlow.OTRS
+{
+    /// <summary>
+    /// Информация по тикету OTRS
+    /// </summary>
+    public class OtrsTicketInfo
+    {
+        public OtrsTicketInfo() { }
+        public OtrsTicketInfo(string id, string number, string name, string state, string workItem)
+        {
+            this.ID = id;
+            this.Number = number;
+            this.Name = name;
+            this.State = state;
+            this.WorkItem = workItem;
+        }
+
+        public string ID { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string WorkItem { get; set; }
+
+
+        public override string ToString()
+        {
+            return string.Format(" Id:\t{0}\n Number:\t{1}\n Name:\t{2}\n State:\t{3}\n WorkItem:\t{4}\n", ID, Number, Name, State, WorkItem);
+        }
+
+
+    }
+}
