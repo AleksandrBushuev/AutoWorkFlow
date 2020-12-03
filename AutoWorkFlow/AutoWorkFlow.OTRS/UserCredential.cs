e999508c9ab5303679d62e188532adc7fde28817
Aleksandr Bushuev <aleksandr.bushuev@indusoft.ru>
2020-12-03 12:24:24 +0500

    Исправление ошибок конфигурации проекта

--- AutoWorkFlow/AutoWorkFlow.OTRS/UserCredential.cs
+++ AutoWorkFlow/AutoWorkFlow.OTRS/UserCredential.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace AutoWorkFlow.OTRS
 {
-    public class Class1
+    /// <summary>
+    /// Учетные данные пользователя
+    /// </summary>
+    public class UserCredential
     {
+        public UserCredential(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+        }
+        public string Login { get; }
+        public string Password { get; }
     }
 }
