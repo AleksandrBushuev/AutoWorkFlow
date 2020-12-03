13d37526a2ba3a804f3e8a78742f0c75a5aac252
Aleksandr Bushuev <aleksandr.bushuev.97@mail.ru>
2020-12-03 13:51:39 +0500

    Создал в консольном приложении метод, позволяющий выполнить проверку авторизации и получение информации по рабочему элементу в TFS

--- AutoWorkFlow/AutoWorkFlow/Program.cs
+++ AutoWorkFlow/AutoWorkFlow/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AutoWorkFlow.OTRS;
+using AutoWorkFlow.TFS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,18 +20,31 @@ namespace AutoWorkFlow
             IAutorizationService service = new AutorizationService(baseAddress, credential);
             OtrsClient client = new OtrsClient(service);
 
-            OtrsTicketInfo info = client.GetTicketInfoAsync(baseAddress, "31627").Result;
+            Console.WriteLine("Id тикета");
+            string id = Console.ReadLine();
+            OtrsTicketInfo info = client.GetTicketInfoAsync(baseAddress, id).Result;
 
             Console.WriteLine(info.ToString());
 
+
+            Console.WriteLine("Личный маркер доступа");
+            string token = Console.ReadLine();
+            TfsClient tfsClient = new TfsClient();
+
+            TfsWorkIteamInfo workIteamInfo = tfsClient.GetWorkIteamInfoAsync("https://tfs.indusoft.ru/tfs/InduSoft/I-DS-RO/", token, info.WorkItem).Result;
+
+            Console.WriteLine(workIteamInfo.ToString());
+
             Console.ReadKey();
         }
+
+
 
         static UserCredential InputUserCredential()
         {
             Console.WriteLine("Введите логин пользователя OTRS");
             string login = Console.ReadLine();
-            Console.WriteLine("Введите логин пользователя OTRS");
+            Console.WriteLine("Введите пароль пользователя OTRS");
             string password = Console.ReadLine();
 
             return new UserCredential(login, password);
