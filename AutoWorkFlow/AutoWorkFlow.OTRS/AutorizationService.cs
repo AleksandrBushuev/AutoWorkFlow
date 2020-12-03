fe01954bd6d20096d6f0f429c80c79b0793f61e3
Aleksandr Bushuev <aleksandr.bushuev@indusoft.ru>
2020-12-03 11:13:22 +0500

    Создал службу авторизации, позволяющую получить cookie авторизации OTRS

---
+++ AutoWorkFlow/AutoWorkFlow.OTRS/AutorizationService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AutoWorkFlow.OTRS
+{
+    /// <summary>
+    /// Служба аторизации OTRS
+    /// </summary>
+    public class AutorizationService : IAutorizationService
+    {
+        private CookieCollection _cookies;
+        private UserCredential _credential;
+
+
+        public AutorizationService(string address, UserCredential credential)
+        {
+            this.Address = address;
+            this._credential = credential;
+            _cookies = new CookieCollection();
+        }
+
+        public string Address { get; }
+
+
+        /// <summary>
+        /// Выполнить авторизацию
+        /// </summary>
+        /// <returns></returns>
+        public CookieCollection Login()
+        {
+
+            if (_cookies.Count == 0)
+            {
+                _cookies = AuthorizeAsync().Result;
+            }
+            return _cookies;
+        }
+
+
+        public void Logout()
+        {
+
+        }
+
+        /// <summary>
+        /// Авторизация в OTRS
+        /// </summary>
+        /// <returns></returns>
+        private async Task<CookieCollection> AuthorizeAsync()
+        {
+            using (var clientHandler = new HttpClientHandler())
+            {
+                using (var client = new HttpClient(clientHandler))
+                {
+                    var uri = new Uri(Address);
+                    var response = await client.PostAsync(uri, CreateBody());
+                    response.EnsureSuccessStatusCode();
+
+                    return clientHandler.CookieContainer.GetCookies(uri);
+                }
+
+            }
+        }
+        /// <summary>
+        /// Создать параметры авторизации в теле POST запроса
+        /// </summary>
+        /// <returns></returns>
+        private HttpContent CreateBody()
+        {
+            return new FormUrlEncodedContent(new[]
+            {
+                    new KeyValuePair<string,string>("Action","Login"),
+                    new KeyValuePair<string,string>("RequestedURL",""),
+                    new KeyValuePair<string,string>("Lang","ru"),
+                    new KeyValuePair<string,string>("TimeOffset","-300"),
+                    new KeyValuePair<string,string>("User", _credential.Login),
+                    new KeyValuePair<string,string>("Password",_credential.Password),
+            });
+        }
+
+
+    }
+}
