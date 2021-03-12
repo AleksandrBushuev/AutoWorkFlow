4e86689c58f255ad5150bee47eb9c158adbb50c9
Aleksandr Bushuev <aleksandr.bushuev.97@mail.ru>
2021-03-12 16:27:10 +0500

    Добавил методы для получения информации по тикетам

--- AutoWorkFlow/AutoWorkFlow.OTRS/OtrsClient.cs
+++ AutoWorkFlow/AutoWorkFlow.OTRS/OtrsClient.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -34,8 +36,72 @@ namespace AutoWorkFlow.OTRS
                 sitePage = await response.Content.ReadAsStringAsync();
             }
             return OtrsTicketInfoParser.convertToOtrsTicketInfo(sitePage);
-
         }
+
+        /// <summary>
+        /// Получить информацию по тикетам
+        /// </summary>
+        /// <param name="baseAddress">Базовый адрес OTRS</param>
+        /// <param name="idTiskets">Идентификаторы тикетов</param>
+        /// <returns></returns>
+        public async Task<List<OtrsTicketInfo>> GetTisketInfoAsync(string baseAddress, List<int> idTiskets)
+        {
+            List<OtrsTicketInfo> ticketInfos = new List<OtrsTicketInfo>();
+            foreach (int id in idTiskets)
+            {
+                var info = await GetTicketInfoAsync(baseAddress, id.ToString());
+                ticketInfos.Add(info);
+            }
+            return ticketInfos;
+        }
+
+
+        /// <summary>
+        /// Получить информацию по тикету
+        /// </summary>
+        /// <param name="baseAddress">Базовый адрес OTRS</param>        
+        /// <returns></returns>
+        public async Task<List<int>> GetTicketsID(string baseAddress)
+        {
+            OtrsTicketAgentParser agentParser = new OtrsTicketAgentParser();
+            List<int> idTiskets = new List<int>();
+            int startHit = 0;
+            int count = 0;
+            do
+            {
+                string page = await LoadAgentTisketAsync(baseAddress, startHit);
+                if (count == 0)
+                {
+                    count = agentParser.GetTicketCount(page);
+                }
+                startHit += 10;
+                idTiskets.AddRange(agentParser.GetTicketsId(page));
+            } while (startHit <= count);
+
+            var result = idTiskets.Distinct().ToList();//удалить дубликаты
+            return result;
+        }            
+       
+
+        /// <summary> 
+        /// Загрузить страницу AgentTicketResponsibleView
+        /// </summary>
+        /// <param name="baseAddress">Базовый адрес OTRS</param>
+        /// <param name="startHit">Начальный номер тикета для загрузки таблицы</param>
+        /// <returns></returns>
+        private async Task<string> LoadAgentTisketAsync(string baseAddress, int startHit)
+        {
+            Uri uri = new Uri($"{baseAddress}?Action=AgentTicketResponsibleView;Filter=All;View=;SortBy=Age;OrderBy=Down;StartWindow=0;StartHit={startHit}");
+            string sitePage;
+            using (var client = CreateClient(baseAddress))
+            {
+                HttpResponseMessage response = await client.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+                sitePage = await response.Content.ReadAsStringAsync();
+            }
+            return sitePage;
+        }
+
 
 
         /// <summary>
@@ -55,22 +121,20 @@ namespace AutoWorkFlow.OTRS
 
         }
 
-        /// <summary>
-        /// Получить cookie в службе авторизации
-        /// </summary>
-        /// <returns></returns>
-        private CookieCollection Login()
-        {
-            return _service.Login();
-        }
+
 
         private CookieContainer CreateCookieContainer()
         {
             CookieContainer container = new CookieContainer();
             container.Add(Login());
             return container;
+        }     
+        
+       
+        private CookieCollection Login()
+        {
+            return _service.Login();//Получить cookie в службе авторизации
         }
-
 
     }
 }
