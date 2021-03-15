d34930a1b96c11174384ae1d37202f7cf79b0b25
Aleksandr Bushuev <aleksandr.bushuev.97@mail.ru>
2021-03-15 16:44:54 +0500

    Добавил класс XmlSerializerHelper

---
+++ AutoWorkFlow.Serializable/XmlSerializerHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AutoWorkFlow.Serializable
+{
+    public class XmlSerializerHelper<T>
+    {
+        public void Serialize(string path, T data, FileMode mode = FileMode.OpenOrCreate)
+        {         
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+            using (FileStream stream = new FileStream(path, mode))
+            {
+                xmlSerializer.Serialize(stream, data);
+            }
+        }
+
+        public T Deserialize(string path, FileMode mode = FileMode.OpenOrCreate)
+        {
+            T data;
+           
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+            using (FileStream stream = new FileStream(path, mode))
+            {
+                data = (T)xmlSerializer.Deserialize(stream);
+            }
+            return data;            
+        }
+
+
+    }
+}
